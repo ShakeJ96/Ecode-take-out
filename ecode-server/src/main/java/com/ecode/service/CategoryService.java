28d265325565a9705de87776bd0337ceea19f877
ShakeJ96 <919052005@qq.com>
2023-12-14 15:44:47 +0800

    12.14 菜品分类和套餐分类的目录管理（类似员工部分的操作）

---
+++ ecode-server/src/main/java/com/ecode/service/CategoryService.java
@@ -0,0 +1,49 @@
+package com.ecode.service;
+
+import com.ecode.dto.CategoryDTO;
+import com.ecode.dto.CategoryPageQueryDTO;
+import com.ecode.entity.Category;
+import com.ecode.result.PageResult;
+import java.util.List;
+
+public interface CategoryService {
+
+    /**
+     * 新增分类
+     * @param categoryDTO
+     */
+    void save(CategoryDTO categoryDTO);
+
+    /**
+     * 分页查询
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(CategoryPageQueryDTO categoryPageQueryDTO);
+
+    /**
+     * 根据id删除分类
+     * @param id
+     */
+    void deleteById(Long id);
+
+    /**
+     * 修改分类
+     * @param categoryDTO
+     */
+    void update(CategoryDTO categoryDTO);
+
+    /**
+     * 启用、禁用分类
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
+
+    /**
+     * 根据类型查询分类
+     * @param type
+     * @return
+     */
+    List<Category> list(Integer type);
+}
