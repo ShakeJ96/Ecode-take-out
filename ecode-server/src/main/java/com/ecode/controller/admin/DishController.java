f512da6df6f7603a5ae44cc5c9335ed85fa98a87
ShakeJ96 <919052005@qq.com>
2023-12-19 15:47:44 +0800

    12,19 缓存菜单业务功能代码开发

--- ecode-server/src/main/java/com/ecode/controller/admin/DishController.java
+++ ecode-server/src/main/java/com/ecode/controller/admin/DishController.java
@@ -11,6 +11,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Set;
@@ -27,6 +29,18 @@ public class DishController {
     @Autowired
     private DishService dishService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 清理缓存的数据
+     * @param pattern
+     */
+    public void cleanCache(String pattern){
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
+    }
+
     /**
      * 新增菜品
      *
@@ -38,6 +52,10 @@ public class DishController {
     public Result save(@RequestBody DishDTO dishDTO) {
         log.info("新增菜品：{}", dishDTO);
         dishService.saveWithFlavor(dishDTO);//后绪步骤开发
+
+        //清理缓存数据
+        String key="dish_"+dishDTO.getCategoryId();
+        cleanCache(key);
         return Result.success();
     }
 
@@ -65,6 +83,9 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids){
         log.info("菜品批量删除：{}",ids);
         dishService.deleteBatch(ids);
+
+        //将所有的菜品缓存数据清理掉，所有以"dish_"开头的key
+        cleanCache("dish_*");
         return Result.success();
     }
 
@@ -82,7 +103,7 @@ public class DishController {
     }
 
     /**
-     * 添加修改菜品的功能
+     * 修改菜品的功能
      * @param dishDTO
      * @return
      */
@@ -91,6 +112,9 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO){
         log.info("修改菜品：{}",dishDTO);
         dishService.updateWithFlavor(dishDTO);
+
+        //将所有的菜品缓存数据清理掉，所有以"dish_"开头的key
+        cleanCache("dish_*");
         return Result.success();
     }
 
@@ -105,6 +129,9 @@ public class DishController {
     public Result saleOrNot(@PathVariable Integer status,Long id){
         log.info("菜品的起售与停售：{},{}",status,id);
         dishService.saleOrNot(status,id);
+
+        //将所有的菜品缓存数据清理掉，所有以"dish_"开头的key
+        cleanCache("dish_*");
         return Result.success();
     }
 
